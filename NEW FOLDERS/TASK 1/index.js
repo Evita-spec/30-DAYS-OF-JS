913d380d64f891135bdfc0aa3be0613ccc0eb402
Evita-spec <89007822+Evita-spec@users.noreply.github.com>
2021-08-20 16:22:18 +0100

    Add files via upload

---
+++ NEW FOLDERS/TASK 1/index.js
@@ -0,0 +1,9 @@
+const person={
+    firstName:"Evita",
+    surname: "Yobana",
+    age:20,
+    sex:"Female",
+    maritalStatus:"single",
+    nationality:"Nigerian",
+}
+console.log("my name is "+ person.firstName,person.surname, "i am "+ person.age, "i am a "+ person.sex, "i am also a "+ person.nationality, "i am "+ person.maritalStatus)
