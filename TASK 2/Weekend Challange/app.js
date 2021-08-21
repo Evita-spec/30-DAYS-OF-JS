ad01f37a8432a9597dc9e534a824e38b64be02ef
Evita-spec <89007822+Evita-spec@users.noreply.github.com>
2021-08-21 15:00:44 +0100

    Add files via upload

---
+++ TASK 2/Weekend Challange/app.js
@@ -0,0 +1,2 @@
+const looks =["beautiful", "dark", "hairy","blue-eyed", "white-teeth", "ombre lips", "elf ears"];
+console.log(looks.length)
---
+++ TASK 2/index.js
@@ -0,0 +1,4 @@
+let quote =[`"you miss 100% of the shots you don't take".-Wayne Gretzky`, ]
+console.log(quote)
+
+
